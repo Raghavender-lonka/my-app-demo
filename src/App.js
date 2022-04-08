0f65001ec1055374a17d1b8b93f211cb35c02770
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-04-08 21:14:34 +0530

    08-04-22

--- src/App.js
+++ src/App.js
@@ -1,57 +1,28 @@
 import React, { useState } from "react";
-import PropFunc from "./components/props/propFunc";
+import Events from "./components/events/Events";
 
-const myList = [
-  {
-    label: "Apple",
-    price: 20,
-    id: 20,
-  },
-  {
-    label: "Banana",
-    price: 10,
-    id: 10,
-  },
-  {
-    label: "Custurd",
-    price: 5,
-    id: 5,
-  },
-];
+// const myList = [
+//   {
+//     label: "Apple",
+//     price: 20,
+//     id: 20,
+//   },
+//   {
+//     label: "Banana",
+//     price: 10,
+//     id: 10,
+//   },
+//   {
+//     label: "Custurd",
+//     price: 5,
+//     id: 5,
+//   },
+// ];
 
 function App() {
-  const [counterArr, setCounterArr] = useState(myList);
-
-  const handleIncrement = (index) => {
-    // const copyCountArr = cloneDeep(counterArr);
-    const copyCountArr = [...counterArr];
-    copyCountArr[index].price =
-      copyCountArr[index].price + counterArr[index].id;
-    setCounterArr(copyCountArr);
-  };
-
-  const handleDecrement = (index) => {
-    const copyCountArr = [...counterArr];
-    copyCountArr[index].price =
-      copyCountArr[index].price - counterArr[index].id;
-    setCounterArr(copyCountArr);
-  };
-
   return (
     <h1 className="App">
-      {counterArr.map((obj, i) => {
-        const { label, price, id } = obj;
-        return (
-          <PropFunc
-            label={label}
-            counter={price}
-            handleIncrement={handleIncrement}
-            handleDecrement={handleDecrement}
-            index={i}
-            key={id}
-          />
-        );
-      })}
+      <Events />
     </h1>
   );
 }
--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 
 const Events = () => {
-  const [value, setValue] = useState("");
-  return <div></div>;
+  const [myValue, setValue] = useState("");
+
+  const handleChange = (e) => {
+    let password = e.target.value.trim();
+    console.log(password.length);
+    setValue(password);
+  };
+  const handleSubmit = () => {
+    setValue("");
+  };
+
+  return (
+    <div>
+      {/* onChange */}
+      <h1>Form</h1>
+      {myValue.trim().length <= 8 ? (
+        <p>Password should be greater than 8</p>
+      ) : myValue.trim().length <= 10 ? (
+        <p>Password Length should be greater than 10</p>
+      ) : (
+        <p>Password is strong</p>
+      )}
+      <input type="text" onChange={handleChange} value={myValue} />
+      <br />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default Events;
