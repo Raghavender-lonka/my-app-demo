6699dc74e22c21eed079ba1854be17e2e1c3f895
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-04-12 19:13:54 +0530

    12-04-22

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React, { useState } from "react";
-import Events from "./components/events/Events";
-
-// const myList = [
-//   {
-//     label: "Apple",
-//     price: 20,
-//     id: 20,
-//   },
-//   {
-//     label: "Banana",
-//     price: 10,
-//     id: 10,
-//   },
-//   {
-//     label: "Custurd",
-//     price: 5,
-//     id: 5,
-//   },
-// ];
+import { dividerClasses } from "@mui/material"
+import React, { useState } from "react"
+import ClassComponent from "./components/ClassComponent/ClassComponent"
+// import Events from "./components/events/Events"
 
 function App() {
-  return (
-    <h1 className="App">
-      <Events />
-    </h1>
-  );
+  const [showCounter, setShowCounter] = useState(true)
+
+  setTimeout(() => {
+    setShowCounter(false)
+  }, 5000)
+  return <div>{showCounter ? <ClassComponent /> : <h1>Bye</h1>}</div>
 }
 
-export default App;
+export default App
