2379fa745252a9b8396313c0d3d8e1c9eb65cdb1
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-04-12 21:07:20 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { dividerClasses } from "@mui/material"
-import React, { useState } from "react"
-import ClassComponent from "./components/ClassComponent/ClassComponent"
-// import Events from "./components/events/Events"
+import React from "react"
+import { Routes } from "react-router-dom"
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
+import Contact from "./Route/Contact"
+import Home from "./Route/Home"
 
 function App() {
-  const [showCounter, setShowCounter] = useState(true)
-
-  setTimeout(() => {
-    setShowCounter(false)
-  }, 5000)
-  return <div>{showCounter ? <ClassComponent /> : <h1>Bye</h1>}</div>
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/home">Home</Link>
+        <br />
+        <Link to="/contact">Contact</Link>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        {/* see navigate and exact path */}
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
-import "./index.css";
-import App from "./App";
+import "./index.css"
+import App from "./App"
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App />);
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
