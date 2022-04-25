bddd4949321dee59add4f2a523b35fd4d7ebd9b3
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-04-25 21:10:15 +0530

    higher order component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 // import React, { useCallback, useEffect, useState } from "react"
 // import MemoReact from "./components/ReactMemo/ReactMemo"
 import React from "react"
-import Comp from "./components/controlled&uncontrolled/Comp"
+import HigherOrder from "./components/HigherOrder/HigherOrder"
+import KeyDown from "./components/HigherOrder/KeyDown"
 
 function App() {
-  // console.log("App")
-  // const [num, setNum] = useState(Math.random())
-  // const [counter, setCounter] = useState(0)
-
-  // const handleChange = useCallback(() => {
-  //   setCounter(counter + 1)
-  // }, [counter])
-
-  // useEffect(() => {
-  //   console.log("Message App")
-  // }, [counter])
-
-  // // useEffect(() => {
-  // //   setInterval(() => {
-  // //     setNum(Math.random().toFixed(2))
-  // //   }, 2000)
-  // // }, [])
-  // return (
-  //   <div>
-  //     <h1>Num: {num} </h1>
-  //     <MemoReact counter={counter} handleChange={handleChange} />
-  //   </div>
-  // )
   return (
     <div>
-      <Comp />
+      <HigherOrder />
+      <KeyDown />
     </div>
   )
 }
--- src/components/HigherOrder/HigherOrder.jsx
+++ src/components/HigherOrder/HigherOrder.jsx
@@ -1,11 +1,15 @@
 import React from "react"
+import IncrementCounterHoc from "./IncrementCounterHoc"
 
-const HigherOrder = () => {
+const HigherOrder = (props) => {
+  const { counter, handleChange } = props
+  // const [counter, setCounter] = useState(0)
   return (
     <div>
-      <h1>Hi</h1>
+      <p> Counter:{counter}</p>
+      <button onClick={handleChange}>Click</button>
     </div>
   )
 }
 
-export default HigherOrder
+export default IncrementCounterHoc(HigherOrder)
